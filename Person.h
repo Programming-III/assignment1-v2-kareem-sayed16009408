3c0d21d4cef0a5cb4ab6cf823ef7a441e5a3f113
kareem-sayed16009408 <kareem.ahmed@student.giu-uni.de>
2025-11-11 09:01:12 +0200

    kareem

--- Person.h
+++ Person.h
@@ -1,19 +1,16 @@
 #ifndef PERSON_H
 #define PERSON_H
-
 #include <string>
 using namespace std;
 
-//#write Person class here
-
-
-
-
-
-
-
-
-
+class Person {
+    private:
+    string name;
+    int id;
+    public:
+    Person(string name, int id);
+    void display();
+};
 
 
 
