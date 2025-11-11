e38b544de2d855e7f8b0a1cb1178bffc130a3c99
kareem-sayed16009408 <kareem.ahmed@student.giu-uni.de>
2025-11-11 09:37:39 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -2,18 +2,25 @@
 // Created by karee on 11/11/2025.
 //
 
-#ifndef STUDENT_H
-#define STUDENT_H
+#ifndef UNTITLED_STUDENT_H
+#define UNTITLED_STUDENT_H
 #include "Person.h"
 #include <string>
 using namespace std;
 
-class student : Person {
+class Student : Person {
 private:
   int yearLevel;
   string major;
   public:
-  student (string name, int id, int yearLevel, string major);
+  Student (string name, int id, int yearLevel, string major);
+  ~Student (){}
   void display();
+  int getYearLevel() {
+    return yearLevel;
+  }
+  string getMajor() {
+    return major;
+  }
 };
-#endif //STUDENT_H
+#endif //UNTITLED_STUDENT_H
