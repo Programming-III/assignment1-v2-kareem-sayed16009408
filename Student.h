64edb9ac1e1a9199c7b883ff044fd08f3b99e8b4
kareem-sayed16009408 <kareem.ahmed@student.giu-uni.de>
2025-11-11 09:00:49 +0200

    kareem

--- Student.h
+++ Student.h
@@ -1,21 +1,19 @@
+//
+// Created by karee on 11/11/2025.
+//
+
 #ifndef STUDENT_H
 #define STUDENT_H
-
 #include "Person.h"
 #include <string>
-#using namespace std;
-//#write student class here
+using namespace std;
 
-
-
-
-
-
-
-
-
-
-
-
-
-#endif
+class student : Person {
+private:
+  int year;
+  string major;
+  public:
+  student (string name, int id, int year, string major);
+  void display();
+};
+#endif //STUDENT_H
