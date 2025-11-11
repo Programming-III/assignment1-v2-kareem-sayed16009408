971a9b249a791eef2e9b242a44cc5e8243408170
kareem-sayed16009408 <kareem.ahmed@student.giu-uni.de>
2025-11-11 09:54:01 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,39 @@
-#ifndef COURSE_H
-#define COURSE_H
-
+#ifndef UNTITLED_COURSE_H
+#define UNTITLED_COURSE_H
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+
+class Course {
+    private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    public:
+    Course(string courseCode, string courseName,int maxStudents, Student *students, int currentStudents );
+    ~Course();
+    void addStudent(const Student& s);
+    void displayInfo();
+    string getCourseCode() {
+        return courseCode;
+    }
+    string getCourseName() {
+        return courseName;
+    }
+    int getMaxStudents() {
+        return maxStudents;
+    }
+    Student* getStudents() {
+        return students;
+    }
+    int getCurrentStudents() {
+        return currentStudents;
+    }
 
 
 
+};
 
-
-
-
-
-
-
-
-
-
-
-#endif
+#endif //UNTITLED_COURSE_H
